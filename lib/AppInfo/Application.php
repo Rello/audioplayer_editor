d11124546668815b952c878fada06777f85c3b30
Rello <github@scherello.de>
2019-06-29 18:17:00 +0200

    cleanup

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -12,42 +12,12 @@
 namespace OCA\audioplayer_editor\AppInfo;
 
 use OCP\AppFramework\App;
-use OCP\IContainer;
 
 class Application extends App {
 
 	public function __construct(array $urlParams = array()) {
 
 		parent::__construct('audioplayer_editor', $urlParams);
-		$container = $this->getContainer();
 
-		$container->registerService(
-			'URLGenerator', function(IContainer $c) {
-				$server = $c->query('ServerContainer');
-				return $server->getURLGenerator();
-			}
-		);
-
-		$container->registerService(
-			'UserId', function() {
-				$user = \OC::$server->getUserSession()->getUser();
-				if ($user) {
-					return $user->getUID();
-				}
-			}
-		);
-
-		$container->registerService(
-			'L10N', function(IContainer $c) {
-				return $c->query('ServerContainer')
-					 ->getL10N($c->query('AppName'));
-			}
-		);
-
-		$container->registerService(
-			'Config', function(IContainer $c) {
-				return $c->getServer()->getConfig();
-			}
-		);
 	}
 }
