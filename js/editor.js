e007415a370d783de6b1a8eabd77526f69959bd6
Rello <github@scherello.de>
2019-06-29 18:17:23 +0200

    jQuery => vanilla JS

--- js/editor.js
+++ js/editor.js
@@ -29,22 +29,25 @@ if (!OCA.Audioplayer.Editor) {
 OCA.Audioplayer.Editor = {
 
     ID3EditorTabView: function () {
-        var trackid = $("#app-sidebar").data('trackid');
+        var trackid = document.getElementById('app-sidebar').dataset.trackid;
 
         OCA.Audioplayer.Sidebar.resetView();
-        $('#tabHeaderID3Editor').addClass('selected');
-        $('#ID3EditorTabView').removeClass('hidden').html('<div style="text-align:center; word-wrap:break-word;" class="get-metadata"><p><img src="' + OC.imagePath('core', 'loading.gif') + '"><br><br></p><p>' + t('audioplayer', 'Reading data') + '</p></div>');
+        document.getElementById('tabHeaderID3Editor').classList.add('selected');
+        document.getElementById('ID3EditorTabView').classList.remove('hidden');
+        document.getElementById('ID3EditorTabView').innerHTML = '<div style="text-align:center; word-wrap:break-word;" class="get-metadata"><p><img src="' + OC.imagePath('core', 'loading.gif') + '"><br><br></p><p>' + t('audioplayer', 'Reading data') + '</p></div>';
 
         $.ajax({
             type: 'GET',
             url: OC.generateUrl('apps/audioplayer/getaudioinfo'),
             data: {trackid: trackid},
             success: function (jsondata) {
-                var html;
+                var table;
                 if (jsondata.status === 'success') {
 
                     var edit_array = ['Title', 'Genre', 'Year', 'Artist', 'Disc', 'Track'];
-                    var table = $('<div>').css('display', 'table').addClass('table');
+                    table = document.createElement('div');
+                    table.style.display = 'table';
+                    table.classList.add('table');
                     var tablerow;
                     var m;
                     var count = 1;
@@ -53,35 +56,38 @@ OCA.Audioplayer.Editor = {
 
                     var audioinfo = jsondata.data;
                     for (m in audioinfo) {
-                        tablerow = $('<div>').css('display', 'table-row');
-                        tablekey = $('<div>').addClass('key').text(t('audioplayer', m));
-                        tablevalue = $('<div>').css({'cursor': 'text'})
-                            .addClass('value')
-                            .text(audioinfo[m]);
-                        if (jQuery.inArray(m, edit_array) !== -1) {
-                            tablevalue.attr({'data-key': m, 'data-trackid': trackid, 'data-value': audioinfo[m]})
-                                .click(OCA.Audioplayer.Editor.editId3Field.bind($this));
+                        if (edit_array.indexOf(m) !== -1) {
+                            tablerow = document.createElement('div');
+                            tablerow.style.display = 'table-row';
+                            tablekey = document.createElement('div');
+                            tablekey.classList.add('key');
+                            tablekey.innerText = t('audioplayer', m);
+                            tablevalue = document.createElement('div');
+                            tablevalue.style.cursor = 'text';
+                            tablevalue.classList.add('value');
+                            tablevalue.innerText = audioinfo[m];
+                            tablevalue.dataset.key = m;
+                            tablevalue.dataset.trackid = trackid;
+                            tablevalue.dataset.value = audioinfo[m];
+                            tablevalue.addEventListener('click', OCA.Audioplayer.Editor.editId3Field.bind($this));
+                            tablerow.appendChild(tablekey);
+                            tablerow.appendChild(tablevalue);
+                            table.appendChild(tablerow);
+                            count++;
                         }
-                        tablerow.append(tablekey).append(tablevalue);
-                        table.append(tablerow);
-                        count++;
                     }
-                    $('#sidebarTitle').html(jsondata.data['Title']);
-                    $('#sidebarMime').html(jsondata.data['MIME type']);
-
                 } else {
-                    html = t('audioplayer', 'No Data');
+                    table = t('audioplayer', 'No Data');
                 }
-
-                $('#ID3EditorTabView').html(table);
+                document.getElementById('ID3EditorTabView').innerHTML = '';
+                document.getElementById('ID3EditorTabView').appendChild(table);
             }
         });
     },
 
-    editId3Field: function(evt){
-        trackId = $(evt.target).attr('data-trackid');
+    editId3Field: function (evt) {
+        var trackid = document.getElementById('app-sidebar').dataset.trackid;
         trackKey = $(evt.target).attr('data-key');
-        //alert(trackId+trackId);
 
         $('.contenteditable').each(function (i, el) {
             $(el).removeAttr('contenteditable')
@@ -92,15 +98,17 @@ OCA.Audioplayer.Editor = {
 
         $(evt.target).attr({'contenteditable': true})
             .addClass('contenteditable')
-            .keypress(function(e){ if(e.which === 13) {
-                $this.saveId3Field(evt);
-            } })
+            .keypress(function (e) {
+                if (e.which === 13) {
+                    $this.saveId3Field(evt);
+                }
+            })
             .unbind("click").append('<span class="icon-checkmark"></span>');
     },
 
 };
 
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     OCA.Audioplayer.Sidebar.registerSidebarTab({
         id: 'tabHeaderID3Editor',
         class: 'ID3EditorTabView',
